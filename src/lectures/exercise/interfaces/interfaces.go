39b1de93a6a37d3ae5917291c9300caddf4000e8
Max Vovk <maksemvovk@gmail.com>
2022-05-18 09:41:39 +0300

    interfaces: add exercise

---
+++ src/lectures/exercise/interfaces/interfaces.go
@@ -0,0 +1,85 @@
+//--Summary:
+//  Create a program that directs vehicles at a mechanic shop
+//  to the correct vehicle lift, based on vehicle size.
+//
+//--Requirements:
+//* The shop has lifts for multiple vehicle sizes/types:
+//  - Motorcycles: small lifts
+//  - Cars: standard lifts
+//  - Trucks: large lifts
+//* Write a single function to handle all of the vehicles
+//  that the shop works on.
+//* Vehicles have a model name in addition to the vehicle type:
+//  - Example: "Truck" is the vehicle type, "Road Devourer" is a model name
+//* Direct at least 1 of each vehicle type to the correct
+//  lift, and print out the vehicle information.
+//
+//--Notes:
+//* Use any names for vehicle models
+
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	SmallLift = iota
+	StandartLift
+	LargeLift
+)
+
+type Lift int
+
+type LiftPicker interface {
+	PickLift() Lift
+}
+
+type Motorcycles string
+type Car string
+type Truck string
+
+func (m Motorcycles) String() string {
+	return fmt.Sprintf("Motorcycles: %v", string(m))
+}
+
+func (c Car) String() string {
+	return fmt.Sprintf("Car: %v", string(c))
+}
+
+func (t Truck) String() string {
+	return fmt.Sprintf("Truck: %v", string(t))
+}
+
+func (m Motorcycles) PickLift() Lift {
+	return SmallLift
+}
+
+func (c Car) PickLift() Lift {
+	return StandartLift
+}
+
+func (t Truck) PickLift() Lift {
+	return LargeLift
+}
+
+func sendToLift(p LiftPicker) {
+	switch p.PickLift() {
+	case SmallLift:
+		fmt.Printf("send %v send to small lift\n", p)
+	case StandartLift:
+		fmt.Printf("send %v send to standart lift\n", p)
+	case LargeLift:
+		fmt.Printf("send %v send to large lift\n", p)
+	}
+}
+
+func main() {
+	car := Car("Sporty")
+	truck := Truck("MountainCrusher")
+	motorcycle := Motorcycles("Croozer")
+
+	sendToLift(car)
+	sendToLift(truck)
+	sendToLift(motorcycle)
+}
